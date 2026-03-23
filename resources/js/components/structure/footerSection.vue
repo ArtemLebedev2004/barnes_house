fa15d587686830af6f5bacb8756f7f1d15bc692b
Artyom Lebedev <artem-lebedev-2004@inbox.ru>
2026-03-23 20:37:18 +0400

    dfdgr

--- resources/js/components/structure/footerSection.vue
+++ resources/js/components/structure/footerSection.vue
@@ -5,8 +5,8 @@
       ">
         <div class="
             m-auto
-            max-w-[450px] min-[768px]:max-w-[520px]
-            px-4.5 py-20
+            max-w-[450px] min-[768px]:max-w-[520px] min-[1024px]:max-w-[1030px]
+            px-4.5 min-[1024px]:px-[30px] py-20
 
         ">
             <div class="relative text-liorange text-[19vw] min-[450px]:text-[85px] min-[768px]:text-[107px] font-black leading-[19vw] min-[450px]:leading-[85px] min-[768px]:leading-[90%] text-center">
@@ -19,18 +19,18 @@
                 </div>
             </div>
 
-            <div class="w-57.25 h-0.5 bg-daorange mt-[11.2%] m-auto">
+            <div class="w-57.25 h-0.5 bg-daorange mt-[11.2%] min-[1024px]:mt-18 m-auto">
 
             </div>
 
-            <div class="flex flex-col gap-[6vw] min-[450px]:gap-[27px] justify-center items-center mt-[11.2%] text-sand uppercase text-[4vw] min-[450px]:text-[18px] min-[768px]:text-[2.5vw] min-[820px]:text-[20.5px] font-bold">
+            <div class="flex flex-col min-[1024px]:flex-row gap-[6vw] min-[450px]:gap-[27px] min-[1024px]:gap-[55px] justify-center items-center mt-[11.2%] min-[1024px]:mt-18 text-sand uppercase text-[4vw] min-[450px]:text-[18px] min-[768px]:text-[2.5vw] min-[820px]:text-[20.5px] min-[1024px]:text-[16px] font-bold">
                 <div>3D-экскурсия по дому</div>
                 <div>Условия проживания</div>
                 <div>Бронирование</div>
                 <div>Наш адрес</div>
             </div>
 
-            <div class="mt-[11.2%]">
+            <div class="mt-[11.2%] min-[1024px]:mt-18 min-[1024px]:flex justify-center gap-18">
                 <a href="tel:89374541087" class="flex gap-[5vw] min-[450px]:gap-[18px] min-[768px]:gap-[32px] items-center">
                     <div class="relative w-[13.5vw] min-[450px]:w-[60.75px] min-[768px]:w-[82px] h-[13.5vw] min-[450px]:h-[60.75px] min-[768px]:h-[82px] rounded-full bg-daorange flex items-center justify-center">
                         <img src="../../../assets/icons/phone.svg" alt="" class="w-[40%]">
@@ -51,7 +51,7 @@
                     </div>
                 </a>
 
-                <a href="mailto:Barneshause73@mail.ru" class="flex gap-[5vw] min-[450px]:gap-[18px] min-[768px]:gap-[32px] items-center mt-[5%] min-[380px]:mt-[7%]">
+                <a href="mailto:Barneshause73@mail.ru" class="flex gap-[5vw] min-[450px]:gap-[18px] min-[768px]:gap-[32px] items-center mt-[5%] min-[380px]:mt-[7%] min-[1024px]:mt-0">
                     <div class="relative w-[13.5vw] min-[450px]:w-[60.75px] min-[768px]:w-[82px]  h-[13.5vw] min-[450px]:h-[60.75px] min-[768px]:h-[82px] rounded-full bg-daorange flex items-center justify-center">
                         <img src="../../../assets/icons/mail.svg" alt="" class="w-[50%]">
                     </div>
@@ -72,7 +72,7 @@
                 </a>
             </div>
 
-            <div class="flex flex-col gap-[6vw] min-[450px]:gap-[26.94px] min-[768px]:gap-[5vw] min-[820px]:gap-[41px] items-center mt-[11.2%] text-[4vw] min-[450px]:text-[18px] min-[768px]:text-[2.5vw] min-[820px]:text-[20.5px] text-sand font-semibold">
+            <div class="min-[1024px]:hidden flex flex-col min-[1024px]:flex-row gap-[6vw] min-[450px]:gap-[26.94px] min-[768px]:gap-[5vw] min-[820px]:gap-[41px] items-center mt-[11.2%] text-[4vw] min-[450px]:text-[18px] min-[768px]:text-[2.5vw] min-[820px]:text-[20.5px] text-sand font-semibold">
                 <div class="uppercase w-max">
                     Сайт создан совой
                     <div class="bg-daorange h-[2px]">
@@ -103,6 +103,41 @@
                         2026
                     </div>
                 </div>
+            </div>
+
+            <div class="hidden min-[1024px]:flex justify-between gap-[6vw] min-[450px]:gap-[26.94px] min-[768px]:gap-[5vw] min-[820px]:gap-[50px] items-center mt-[11.2%] text-[4vw] min-[450px]:text-[18px] min-[768px]:text-[2.5vw] min-[820px]:text-[20.5px] min-[1024px]:text-[16px] text-sand font-semibold">
+                <div class="uppercase w-max">
+                    Сайт создан совой
+                    <div class="bg-daorange h-[2px]">
+                    </div>
+                </div>
+
+                <div class=" w-max flex items-center gap-[1vw] min-[450px]:gap-[4.49px]">
+                    <div>
+                        ©
+                    </div>
+
+                    <div class="flex leading-[100%]">
+                        <div>
+                            barneshouse73.ru
+                            <div class="bg-daorange h-[2px]">
+                            </div>
+                        </div>
+                        ,
+                    </div>
+
+                    <div>
+                        2026
+                    </div>
+                </div>
+
+                <div class="uppercase w-max">
+                    ЮРИДИЧЕСКАЯ ИНФОРМАЦИЯ
+                    <div class="bg-daorange h-[2px]">
+                    </div>
+                </div>
+
+               
             </div>
         </div>
 
